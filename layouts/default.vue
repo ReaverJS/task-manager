b2d73c68a9148f5bbeb1c94043cb2c962b437f6d
Fakhrutdinov, Abubakir <bitrix@smartdigital.kz>
2021-10-28 23:03:31 +0600

    test layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,50 @@
 <template>
-	<div>
-		<Nuxt/>
+	<div class="app">
+		<LayoutWidget position="top"></LayoutWidget>
+
+		<div class="main-wrapper">
+			<LayoutWidget position="left"></LayoutWidget>
+
+			<main>
+				<Nuxt/>
+			</main>
+
+			<LayoutWidget position="right"></LayoutWidget>
+		</div>
+
+		<LayoutWidget position="bottom"></LayoutWidget>
+
 	</div>
 </template>
 
-<style>
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.app {
+	background-color: $dark;
+	height: 100vh;
+	display: flex;
+	flex-flow: column;
+	overflow: hidden;
+}
+
+.main-wrapper {
+	padding: $margin;
+	width: 100%;
+	height: 100%;
+	display: flex;
+}
+
+main {
+	border-radius: $radius;
+	background-color: $light;
+	padding: $margin;
+	height: 100%;
+	width: 100%;
+}
 
 </style>
